437a641038b94c28a11666ddd3c8090ca10f4c0d
oGabrielArruda <gabrielalvesdearruda@gmail.com>
2020-04-10 11:34:10 -0300

    initial main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,18 +2,40 @@ import WebService.ClienteWS;
 import InfoCidade.InfoCidade;
 import Logradouro.Logradouro;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main
 {
     public static void main(String[] args)
     {
+        BufferedReader Teclado = new BufferedReader(new InputStreamReader(System.in));
+        int opcaoDesejada = 0;
         try
         {
-            Logradouro logradouro = (Logradouro) ClienteWS.getObjeto(Logradouro.class, "http://api.postmon.com.br/v1/cep", "13035350");
-            System.out.println (logradouro);
+            do{
+                System.out.println("CRUD EXAMPLE LOJAS");
+                System.out.println("Selecione a opção desejada");
+                System.out.println("1- Inserir loja    (Create)");
+                System.out.println("2- Selecionar loja (Read)");
+                System.out.println("3- Alterar loja    (Update)");
+                System.out.println("4- Excluir loja    (Delete)");
+                System.out.println("99- Excluir loja");
+                opcaoDesejada = Integer.parseInt(Teclado.readLine());
+                if(opcaoDesejada < 0 || opcaoDesejada > 4)
+                    continue;
+
+                switch (opcaoDesejada)
+                {
+                    case 1:
+                }
+
+            } while (opcaoDesejada != 99);
+
         }
-        catch (Exception erro)
+        catch (Exception ex)
         {
-            System.err.println (erro.getMessage());
+              ex.printStackTrace();
         }
     }
 }
