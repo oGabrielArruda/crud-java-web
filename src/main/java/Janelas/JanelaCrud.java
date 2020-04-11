9cb5269fb413d24b94a97f84294e7abec92c0685
oGabrielArruda <gabrielalvesdearruda@gmail.com>
2020-04-11 11:09:12 -0300

    janela

---
+++ src/main/java/Janelas/JanelaCrud.java
@@ -0,0 +1,40 @@
+package Janelas;
+
+import javax.swing.*;
+
+public class JanelaCrud extends JFrame{
+    private JTextField códigoLojaTextField;
+    private JTextField textField2;
+    private JTextField nomeTextField;
+    private JTextField textField4;
+    private JTextField cepTextField;
+    private JTextField textField6;
+    private JTextField númeroTextField;
+    private JTextField textField8;
+    private JTextField complementoTextField;
+    private JTextField textField10;
+    private JTextField textField11;
+    private JTextField ruaTextField;
+    private JTextField bairroTextField;
+    private JTextField textField14;
+    private JTextField cidadeTextField;
+    private JTextField textField16;
+    private JTextField estadoTextField;
+    private JTextField textField18;
+    private JTextField categoriaTextField;
+    private JTextField textField20;
+    private JButton inserirButton;
+    private JButton procurarButton;
+    private JButton alterarButton;
+    private JButton deletarButton;
+    private JTextField qtdFuncionáriosTextField;
+    private JTextField textField3;
+    private JPanel rootPanel;
+
+    public JanelaCrud()
+    {
+        add(rootPanel);
+        setSize(500, 500);
+        setTitle("Loja crud");
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import Janelas.JanelaCrud;
 import WebService.ClienteWS;
 import InfoCidade.InfoCidade;
 import Logradouro.Logradouro;
@@ -9,33 +10,14 @@ public class Main
 {
     public static void main(String[] args)
     {
-        BufferedReader Teclado = new BufferedReader(new InputStreamReader(System.in));
-        int opcaoDesejada = 0;
         try
         {
-            do{
-                System.out.println("CRUD EXAMPLE LOJAS");
-                System.out.println("Selecione a opção desejada");
-                System.out.println("1- Inserir loja    (Create)");
-                System.out.println("2- Selecionar loja (Read)");
-                System.out.println("3- Alterar loja    (Update)");
-                System.out.println("4- Excluir loja    (Delete)");
-                System.out.println("99- Excluir loja");
-                opcaoDesejada = Integer.parseInt(Teclado.readLine());
-                if(opcaoDesejada < 0 || opcaoDesejada > 4)
-                    continue;
-
-                switch (opcaoDesejada)
-                {
-                    case 1:
-                }
-
-            } while (opcaoDesejada != 99);
-
+            JanelaCrud janela = new JanelaCrud();
+            janela.setVisible(true);
         }
-        catch (Exception ex)
+        catch (Exception e)
         {
-              ex.printStackTrace();
+            e.printStackTrace();
         }
     }
 }
